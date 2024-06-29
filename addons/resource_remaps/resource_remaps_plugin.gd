98fb5e10638acfedb92e49718b0f374c0a2a6ba1
Allen Pestaluky <allenpestaluky@gmail.com>
2024-06-29 12:58:33 -0400

    Added control to project settings for resource remaps

--- addons/resource_remaps/resource_remaps_plugin.gd
+++ addons/resource_remaps/resource_remaps_plugin.gd
@@ -1,12 +1,18 @@
 @tool
 extends EditorPlugin
 
+# TODO:
+# - Move tab to the far right? Shouldn't this be already working?
+
+const CONTROL_CONTAINER = CONTAINER_PROJECT_SETTING_TAB_RIGHT
+var control: Control
 
 func _enter_tree() -> void:
 	# Initialization of the plugin goes here.
-	pass
-
+	control = preload("res://addons/resource_remaps/resource_remaps_control.tscn").instantiate()
+	add_control_to_container(CONTROL_CONTAINER, control)
 
 func _exit_tree() -> void:
 	# Clean-up of the plugin goes here.
-	pass
+	remove_control_from_container(CONTROL_CONTAINER, control)
+	control.free()
