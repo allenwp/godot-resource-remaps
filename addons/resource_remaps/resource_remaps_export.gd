5fbe96aea7195b2c077986b40836d7bf76e204d4
Allen Pestaluky <allenpestaluky@gmail.com>
2024-07-08 12:37:45 -0400

    Reworked mapping to use arrays that can be reordered easily. Also added a workaround to Godot issue #94045: Don't skip textures that have been remapped to another.

--- addons/resource_remaps/resource_remaps_export.gd
+++ addons/resource_remaps/resource_remaps_export.gd
@@ -21,15 +21,27 @@ func _export_begin(features: PackedStringArray, _is_debug: bool, _path: String, 
 
 	# Temporary while I figure out the GUI for this:
 	remap_resource = {
-		"res://icon-default.svg": { "mobile": "res://icon-mobile.svg" },
-		"res://noise-pc.png": { "mobile": "res://noise-mobile.png" },
-		"res://audio-default.wav": { "mobile": "res://audio-mobile.wav" }
+		"res://icon-default.svg": [
+			["ios", "res://icon-ios.svg"],
+			["mobile", "res://icon-mobile.svg"],
+		],
+		"res://noise-pc.png": [
+			["mobile", "res://noise-mobile.png"],
+		],
+		"res://audio-default.wav": [
+			["mobile", "res://audio-mobile.wav"],
+		]
 	}
 	remap_scene = {
-		"res://default.tscn": { "mobile": "res://mobile.tscn" }
+		"res://default.tscn": [
+			["ios", "res://ios.tscn"],
+			["mobile", "res://mobile.tscn"],
+		]
 	}
 	remap_file = {
-		"res://default.ogv": { "mobile": "res://mobile.ogv" }
+		"res://default.ogv": [
+			["mobile", "res://mobile.ogv"],
+		]
 	}
 
 func _begin_customize_resources (_platform: EditorExportPlatform, _f: PackedStringArray) -> bool:
@@ -37,10 +49,11 @@ func _begin_customize_resources (_platform: EditorExportPlatform, _f: PackedStri
 
 func _customize_resource(_resource: Resource, path: String) -> Resource:
 	if remap_resource.has(path):
-		var feature_to_new_path: Dictionary = remap_resource[path]
-		for feature: String in feature_to_new_path.keys():
+		var feature_arrays: Array = remap_resource[path]
+		for feature_array: Array in feature_arrays:
+			var feature: String = feature_array[0]
 			if _features.has(feature):
-				var new_path: String = feature_to_new_path[feature]
+				var new_path: String = feature_array[1]
 				print("[Resource Remap] Resource: ", path, " to: ", new_path)
 				return load(new_path)
 	return null
@@ -50,10 +63,11 @@ func _begin_customize_scenes(_platform: EditorExportPlatform, _f: PackedStringAr
 
 func _customize_scene(scene: Node, path: String) -> Node:
 	if remap_scene.has(path):
-		var feature_to_new_path: Dictionary = remap_scene[path]
-		for feature: String in feature_to_new_path.keys():
+		var feature_arrays: Array = remap_scene[path]
+		for feature_array: Array in feature_arrays:
+			var feature: String = feature_array[0]
 			if _features.has(feature):
-				var new_path: String = feature_to_new_path[feature]
+				var new_path: String = feature_array[1]
 				var packed_scene: PackedScene = load(new_path as String) as PackedScene
 				if packed_scene != null && packed_scene.can_instantiate():
 					print("[Resource Remap] Scene: ", path, " to: ", new_path)
@@ -68,31 +82,50 @@ func _customize_scene(scene: Node, path: String) -> Node:
 
 func _export_file(path: String, _type: String, features: PackedStringArray) -> void:
 	if remap_file.has(path):
-		var feature_to_new_path: Dictionary = remap_file[path]
-		for feature: String in feature_to_new_path.keys():
+		var feature_arrays: Array = remap_file[path]
+		for feature_array: Array in feature_arrays:
+			var feature: String = feature_array[0]
 			if features.has(feature):
-				var new_path: String = feature_to_new_path[feature]
+				var new_path: String = feature_array[1]
 				print("[Resource Remap] File: ", path, " to: ", new_path)
 				add_file(path, FileAccess.get_file_as_bytes(new_path as String), true)
 				break
 
+	# Workaround to Godot issue #94045: Don't skip textures that have been remapped to another
+	if _type == "CompressedTexture2D":
+		for feature_arrays: Array in remap_resource.values():
+			for feature_array: Array in feature_arrays:
+				var feature: String = feature_array[0]
+				if _features.has(feature):
+					var new_path: String = feature_array[1]
+					if new_path == path:
+						print("[Resource Remap] NOT skipping CompressedTexture2D (.ctex file) because it's referenced by another resource. ", path)
+						return
+					else:
+						# We've found the first valid feature mapping for this remap and it's not
+						# this path. Keep looking...
+						break
+
 	# Skip all files that are overrides:
-	for feature_to_new_path: Dictionary in remap_resource.values():
-		for remapped_path: String in feature_to_new_path.values():
+	for feature_arrays: Array in remap_resource.values():
+		for feature_array: Array in feature_arrays:
+			var remapped_path: String = feature_array[1]
 			if remapped_path == path:
 				print("[Resource Remap] Skipping resource because it has been remapped: ", path)
 				skip()
 				return
 
-	for feature_to_new_path: Dictionary in remap_scene.values():
-		for remapped_path: String in feature_to_new_path.values():
+	for feature_arrays: Array in remap_scene.values():
+		for feature_array: Array in feature_arrays:
+			var remapped_path: String = feature_array[1]
 			if remapped_path == path:
 				print("[Resource Remap] Skipping scene because it has been remapped: ", path)
 				skip()
 				return
 
-	for feature_to_new_path: Dictionary in remap_file.values():
-		for remapped_path: String in feature_to_new_path.values():
+	for feature_arrays: Array in remap_file.values():
+		for feature_array: Array in feature_arrays:
+			var remapped_path: String = feature_array[1]
 			if remapped_path == path:
 				print("[Resource Remap] Skipping file because it has been remapped: ", path)
 				skip()
