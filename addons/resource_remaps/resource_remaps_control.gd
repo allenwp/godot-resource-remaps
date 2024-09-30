f5610f9dd1c93b4f27a2fed39153756d07adfcdc
Allen Pestaluky <allenpestaluky@gmail.com>
2024-09-30 14:42:40 -0400

    Renamed translation to resource remap

--- addons/resource_remaps/resource_remaps_control.gd
+++ addons/resource_remaps/resource_remaps_control.gd
@@ -1,15 +1,15 @@
 class_name ResourceRemapControl extends VBoxContainer
 
-var translation_list: Tree = null
+var res_remap_option_add_button: Button = null
+var res_remap_file_open_dialog: EditorFileDialog = null
+var res_remap_option_file_open_dialog: EditorFileDialog = null
+## List of resources
+var res_remap: Tree = null
+## List of remaps for selected resource
+var res_remap_options: Tree = null
 
-var translation_res_option_add_button: Button = null
-var translation_res_file_open_dialog: EditorFileDialog = null
-var translation_res_option_file_open_dialog: EditorFileDialog = null
-var translation_remap: Tree = null
-var translation_remap_options: Tree = null
-
-var updating_translations: bool = false
-var localization_changed: String
+var updating_res_remaps: bool = false
+var localization_changed: String = "localization_changed"
 
 var undo_redo: UndoRedo = UndoRedo.new()
 
@@ -22,13 +22,13 @@ func _notification(p_what: int) -> void:
 		NOTIFICATION_ENTER_TREE:
 			var rfn: PackedStringArray = ResourceLoader.get_recognized_extensions_for_type("Resource")
 			for E: String in rfn:
-				translation_res_file_open_dialog.add_filter("*." + E)
-				translation_res_option_file_open_dialog.add_filter("*." + E)
+				res_remap_file_open_dialog.add_filter("*." + E)
+				res_remap_option_file_open_dialog.add_filter("*." + E)
 
-func _translation_res_file_open() -> void:
-	translation_res_file_open_dialog.popup_file_dialog()
+func _res_remap_file_open() -> void:
+	res_remap_file_open_dialog.popup_file_dialog()
 
-func _translation_res_add(p_paths: PackedStringArray) -> void:
+func _res_remap_add(p_paths: PackedStringArray) -> void:
 	var prev: Variant
 	var remaps: Dictionary
 
@@ -41,25 +41,25 @@ func _translation_res_add(p_paths: PackedStringArray) -> void:
 			# Don't overwrite with an empty remap array if an array already exists for the given path.
 			remaps[path] = PackedStringArray()
 
-	undo_redo.create_action(TTR("Translation Resource Remap: Add %d Path(s)") % p_paths.size())
+	undo_redo.create_action(TTR("Resource Remap: Add %d Path(s)") % p_paths.size())
 	undo_redo.add_do_property(ProjectSettings, "internationalization/locale/translation_remaps", remaps)
 	undo_redo.add_undo_property(ProjectSettings, "internationalization/locale/translation_remaps", prev)
-	undo_redo.add_do_method(update_translations)
-	undo_redo.add_undo_method(update_translations)
+	undo_redo.add_do_method(update_res_remaps)
+	undo_redo.add_undo_method(update_res_remaps)
 	undo_redo.add_do_method(emit_localization_changed_signal)
 	undo_redo.add_undo_method(emit_localization_changed_signal)
 	undo_redo.commit_action()
 
-func _translation_res_option_file_open() -> void:
-	translation_res_option_file_open_dialog.popup_file_dialog()
+func _res_remap_option_file_open() -> void:
+	res_remap_option_file_open_dialog.popup_file_dialog()
 
-func _translation_res_option_add(p_paths: PackedStringArray) -> void:
+func _res_remap_option_add(p_paths: PackedStringArray) -> void:
 	if !ProjectSettings.has_setting("internationalization/locale/translation_remaps"):
 		return
 
 	var remaps: Dictionary = ProjectSettings.get_setting("internationalization/locale/translation_remaps")
 
-	var k: TreeItem = translation_remap.get_selected()
+	var k: TreeItem = res_remap.get_selected()
 	if k == null:
 		return
 
@@ -73,40 +73,40 @@ func _translation_res_option_add(p_paths: PackedStringArray) -> void:
 		r.append(path + ":en")
 	remaps[key] = r
 
-	undo_redo.create_action(TTR("Translation Resource Remap: Add %d Remap(s)") % p_paths.size())
+	undo_redo.create_action(TTR("Resource Remap: Add %d Remap(s)") % p_paths.size())
 	undo_redo.add_do_property(ProjectSettings, "internationalization/locale/translation_remaps", remaps)
 	undo_redo.add_undo_property(ProjectSettings, "internationalization/locale/translation_remaps", ProjectSettings.get_setting("internationalization/locale/translation_remaps"))
-	undo_redo.add_do_method(update_translations)
-	undo_redo.add_undo_method(update_translations)
+	undo_redo.add_do_method(update_res_remaps)
+	undo_redo.add_undo_method(update_res_remaps)
 	undo_redo.add_do_method(emit_localization_changed_signal)
 	undo_redo.add_undo_method(emit_localization_changed_signal)
 	undo_redo.commit_action()
 
-func _translation_res_select() -> void:
-	if updating_translations:
+func _res_remap_select() -> void:
+	if updating_res_remaps:
 		return
-	call_deferred("update_translations")
+	call_deferred("update_res_remaps")
 
-func _translation_res_option_popup(_p_arrow_clicked: bool) -> void:
-	var ed: TreeItem = translation_remap_options.get_edited()
+func _res_remap_option_popup(_p_arrow_clicked: bool) -> void:
+	var ed: TreeItem = res_remap_options.get_edited()
 	if ed == null:
 		return
 
 	# TODO: For localizaition, this would pop up a whole new dialog for selecting your locale.
 	# For resource remaps, this should just be a pre-populated feature drop-down.
 
-func _translation_res_option_selected(p_locale: String) -> void:
-	var ed: TreeItem = translation_remap_options.get_edited()
+func _res_remap_option_selected(p_locale: String) -> void:
+	var ed: TreeItem = res_remap_options.get_edited()
 	if ed == null:
 		return
 
 	ed.set_text(1, TranslationServer.get_locale_name(p_locale))
 	ed.set_tooltip_text(1, p_locale)
 
-	_translation_res_option_changed()
+	_res_remap_option_changed()
 
-func _translation_res_option_changed() -> void:
-	if updating_translations:
+func _res_remap_option_changed() -> void:
+	if updating_res_remaps:
 		return
 
 	if !ProjectSettings.has_setting("internationalization/locale/translation_remaps"):
@@ -114,10 +114,10 @@ func _translation_res_option_changed() -> void:
 
 	var remaps: Dictionary = ProjectSettings.get_setting("internationalization/locale/translation_remaps")
 
-	var k: TreeItem = translation_remap.get_selected()
+	var k: TreeItem = res_remap.get_selected()
 	if k == null:
 		return
-	var ed: TreeItem = translation_remap_options.get_edited()
+	var ed: TreeItem = res_remap_options.get_edited()
 	if ed == null:
 		return
 
@@ -132,20 +132,20 @@ func _translation_res_option_changed() -> void:
 	r[idx] = path + ":" + locale
 	remaps[key] = r
 
-	updating_translations = true
+	updating_res_remaps = true
 
 	undo_redo.create_action(TTR("Change Resource Remap Language"))
 	undo_redo.add_do_property(ProjectSettings, "internationalization/locale/translation_remaps", remaps)
 	undo_redo.add_undo_property(ProjectSettings, "internationalization/locale/translation_remaps", ProjectSettings.get_setting("internationalization/locale/translation_remaps"))
-	undo_redo.add_do_method(update_translations)
-	undo_redo.add_undo_method(update_translations)
+	undo_redo.add_do_method(update_res_remaps)
+	undo_redo.add_undo_method(update_res_remaps)
 	undo_redo.add_do_method(emit_localization_changed_signal)
 	undo_redo.add_undo_method(emit_localization_changed_signal)
 	undo_redo.commit_action()
-	updating_translations = false
+	updating_res_remaps = false
 
-func _translation_res_delete(p_item: Object, p_column: int, p_button: int, p_mouse_button: int) -> void:
-	if updating_translations:
+func _res_remap_delete(p_item: Object, p_column: int, p_button: int, p_mouse_button: int) -> void:
+	if updating_res_remaps:
 		return
 
 	#TODO:
@@ -169,14 +169,14 @@ func _translation_res_delete(p_item: Object, p_column: int, p_button: int, p_mou
 	#undo_redo.create_action(TTR("Remove Resource Remap"))
 	#undo_redo.add_do_property(ProjectSettings, "internationalization/locale/translation_remaps", remaps)
 	#undo_redo.add_undo_property(ProjectSettings, "internationalization/locale/translation_remaps", ProjectSettings.get_setting("internationalization/locale/translation_remaps"))
-	#undo_redo.add_do_method(update_translations)
-	#undo_redo.add_undo_method(update_translations)
+	#undo_redo.add_do_method(update_res_remaps)
+	#undo_redo.add_undo_method(update_res_remaps)
 	#undo_redo.add_do_method(emit_localization_changed_signal)
 	#undo_redo.add_undo_method(emit_localization_changed_signal)
 	#undo_redo.commit_action()
 
-func _translation_res_option_delete(p_item: Object, p_column: int, p_button: int, p_mouse_button: MouseButton) -> void:
-	if updating_translations:
+func _res_remap_option_delete(p_item: Object, p_column: int, p_button: int, p_mouse_button: MouseButton) -> void:
+	if updating_res_remaps:
 		return
 
 	#TODO:
@@ -188,7 +188,7 @@ func _translation_res_option_delete(p_item: Object, p_column: int, p_button: int
 #
 	#var remaps: Dictionary = ProjectSettings.get_setting("internationalization/locale/translation_remaps")
 #
-	#var k: TreeItem = translation_remap.get_selected()
+	#var k: TreeItem = res_remap.get_selected()
 	#if k == null:
 		#return
 	#var ed: TreeItem = p_item as TreeItem
@@ -209,8 +209,8 @@ func _translation_res_option_delete(p_item: Object, p_column: int, p_button: int
 	#undo_redo.create_action(TTR("Remove Resource Remap Option"))
 	#undo_redo.add_do_property(ProjectSettings, "internationalization/locale/translation_remaps", remaps)
 	#undo_redo.add_undo_property(ProjectSettings, "internationalization/locale/translation_remaps", ProjectSettings.get_setting("internationalization/locale/translation_remaps"))
-	#undo_redo.add_do_method(update_translations)
-	#undo_redo.add_undo_method(update_translations)
+	#undo_redo.add_do_method(update_res_remaps)
+	#undo_redo.add_undo_method(update_res_remaps)
 	#undo_redo.add_do_method(emit_localization_changed_signal)
 	#undo_redo.add_undo_method(emit_localization_changed_signal)
 	#undo_redo.commit_action()
@@ -258,7 +258,7 @@ func _filesystem_files_moved(p_old_file: String, p_new_file: String) -> void:
 
 	if remaps_changed:
 		ProjectSettings.set_setting("internationalization/locale/translation_remaps", remaps)
-		update_translations()
+		update_res_remaps()
 		emit_localization_changed_signal()
 
 func _filesystem_file_removed(p_file: String) -> void:
@@ -287,27 +287,27 @@ func _filesystem_file_removed(p_file: String) -> void:
 		print_verbose("Remap key \"%s\" has been removed from the file system." % p_file)
 
 	if remaps_changed:
-		update_translations()
+		update_res_remaps()
 		emit_localization_changed_signal()
 
-func update_translations() -> void:
-	if updating_translations:
+func update_res_remaps() -> void:
+	if updating_res_remaps:
 		return
 
-	updating_translations = true
+	updating_res_remaps = true
 
-	# Update translation remaps.
+	# Update resource remaps.
 	var remap_selected: String
-	if translation_remap.get_selected():
-		remap_selected = translation_remap.get_selected().get_metadata(0)
+	if res_remap.get_selected():
+		remap_selected = res_remap.get_selected().get_metadata(0)
 
-	translation_remap.clear()
-	translation_remap_options.clear()
-	var root: TreeItem = translation_remap.create_item()
-	var root2: TreeItem = translation_remap_options.create_item()
-	translation_remap.set_hide_root(true)
-	translation_remap_options.set_hide_root(true)
-	translation_res_option_add_button.disabled = true
+	res_remap.clear()
+	res_remap_options.clear()
+	var root: TreeItem = res_remap.create_item()
+	var root2: TreeItem = res_remap_options.create_item()
+	res_remap.set_hide_root(true)
+	res_remap_options.set_hide_root(true)
+	res_remap_option_add_button.disabled = true
 
 	if ProjectSettings.has_setting("internationalization/locale/translation_remaps"):
 		var remaps: Dictionary = ProjectSettings.get_setting("internationalization/locale/translation_remaps")
@@ -315,7 +315,7 @@ func update_translations() -> void:
 		keys.sort()
 
 		for key:String in keys:
-			var t: TreeItem = translation_remap.create_item(root)
+			var t: TreeItem = res_remap.create_item(root)
 			t.set_editable(0, false)
 			t.set_text(0, key.replace("res://", ""))
 			t.set_tooltip_text(0, key)
@@ -331,7 +331,7 @@ func update_translations() -> void:
 
 			if key == remap_selected:
 				t.select(0)
-				translation_res_option_add_button.disabled = false
+				res_remap_option_add_button.disabled = false
 
 				var selected: PackedStringArray = remaps[key]
 				for j in range(selected.size()):
@@ -340,7 +340,7 @@ func update_translations() -> void:
 					var path: String = s2.substr(0, qp)
 					var locale: String = s2.substr(qp + 1, s2.length())
 
-					var t2: TreeItem = translation_remap_options.create_item(root2)
+					var t2: TreeItem = res_remap_options.create_item(root2)
 					t2.set_editable(0, false)
 					t2.set_text(0, path.replace("res://", ""))
 					t2.set_tooltip_text(0, path)
@@ -358,11 +358,10 @@ func update_translations() -> void:
 						#t2.set_text(0, t2.get_text(0) + " (" + TTR("Removed") + ")")
 						# t2.set_tooltip_text(0, str([t2->get_tooltip_text(0), TTR(" cannot be found.")])
 
-	updating_translations = false
+	updating_res_remaps = false
 
 func _init() -> void:
 	name = TTR("Resource Remaps")
-	localization_changed = "localization_changed"
 
 	var container: MarginContainer = MarginContainer.new()
 	container.size_flags_vertical = Control.SIZE_EXPAND_FILL
@@ -382,27 +381,27 @@ func _init() -> void:
 
 	var addtr: Button = Button.new()
 	addtr.text = TTR("Add...")
-	addtr.pressed.connect(_translation_res_file_open)
+	addtr.pressed.connect(_res_remap_file_open)
 	thb.add_child(addtr)
 
 	var tmc: VBoxContainer = VBoxContainer.new()
 	tmc.size_flags_vertical = Control.SIZE_EXPAND_FILL
 	tvb.add_child(tmc)
 
-	translation_remap = Tree.new()
-	translation_remap.size_flags_vertical = Control.SIZE_EXPAND_FILL
-	translation_remap.cell_selected.connect(_translation_res_select)
-	translation_remap.button_clicked.connect(_translation_res_delete)
-	tmc.add_child(translation_remap)
+	res_remap = Tree.new()
+	res_remap.size_flags_vertical = Control.SIZE_EXPAND_FILL
+	res_remap.cell_selected.connect(_res_remap_select)
+	res_remap.button_clicked.connect(_res_remap_delete)
+	tmc.add_child(res_remap)
 
-	translation_res_file_open_dialog = EditorFileDialog.new()
-	translation_res_file_open_dialog.file_mode = EditorFileDialog.FILE_MODE_OPEN_FILES
-	translation_res_file_open_dialog.files_selected.connect(_translation_res_add)
-	add_child(translation_res_file_open_dialog)
+	res_remap_file_open_dialog = EditorFileDialog.new()
+	res_remap_file_open_dialog.file_mode = EditorFileDialog.FILE_MODE_OPEN_FILES
+	res_remap_file_open_dialog.files_selected.connect(_res_remap_add)
+	add_child(res_remap_file_open_dialog)
 
 	thb = HBoxContainer.new()
 	l = Label.new()
-	l.text = TTR("Remaps by Locale:")
+	l.text = TTR("Remaps by Feature:")
 	l.theme_type_variation = "HeaderSmall"
 	thb.add_child(l)
 	thb.add_spacer(false)
@@ -410,34 +409,34 @@ func _init() -> void:
 
 	addtr = Button.new()
 	addtr.text = TTR("Add...")
-	addtr.pressed.connect(_translation_res_option_file_open)
-	translation_res_option_add_button = addtr
+	addtr.pressed.connect(_res_remap_option_file_open)
+	res_remap_option_add_button = addtr
 	thb.add_child(addtr)
 
 	tmc = VBoxContainer.new()
 	tmc.size_flags_vertical = Control.SIZE_EXPAND_FILL
 	tvb.add_child(tmc)
 
-	translation_remap_options = Tree.new()
-	translation_remap_options.size_flags_vertical = Control.SIZE_EXPAND_FILL
-	translation_remap_options.columns = 2
-	translation_remap_options.set_column_title(0, TTR("Path"))
-	translation_remap_options.set_column_title(1, TTR("Locale"))
-	translation_remap_options.column_titles_visible = true
-	translation_remap_options.set_column_expand(0, true)
-	translation_remap_options.set_column_clip_content(0, true)
-	translation_remap_options.set_column_expand(1, false)
-	translation_remap_options.set_column_clip_content(1, false)
-	translation_remap_options.set_column_custom_minimum_width(1, 250)
-	translation_remap_options.item_edited.connect(_translation_res_option_changed)
-	translation_remap_options.button_clicked.connect(_translation_res_option_delete)
-	translation_remap_options.custom_popup_edited.connect(_translation_res_option_popup)
-	tmc.add_child(translation_remap_options)
+	res_remap_options = Tree.new()
+	res_remap_options.size_flags_vertical = Control.SIZE_EXPAND_FILL
+	res_remap_options.columns = 2
+	res_remap_options.set_column_title(0, TTR("Path"))
+	res_remap_options.set_column_title(1, TTR("Feature"))
+	res_remap_options.column_titles_visible = true
+	res_remap_options.set_column_expand(0, true)
+	res_remap_options.set_column_clip_content(0, true)
+	res_remap_options.set_column_expand(1, false)
+	res_remap_options.set_column_clip_content(1, false)
+	res_remap_options.set_column_custom_minimum_width(1, 250)
+	res_remap_options.item_edited.connect(_res_remap_option_changed)
+	res_remap_options.button_clicked.connect(_res_remap_option_delete)
+	res_remap_options.custom_popup_edited.connect(_res_remap_option_popup)
+	tmc.add_child(res_remap_options)
 
-	translation_res_option_file_open_dialog = EditorFileDialog.new()
-	translation_res_option_file_open_dialog.file_mode = EditorFileDialog.FILE_MODE_OPEN_FILES
-	translation_res_option_file_open_dialog.files_selected.connect(_translation_res_option_add)
-	add_child(translation_res_option_file_open_dialog)
+	res_remap_option_file_open_dialog = EditorFileDialog.new()
+	res_remap_option_file_open_dialog.file_mode = EditorFileDialog.FILE_MODE_OPEN_FILES
+	res_remap_option_file_open_dialog.files_selected.connect(_res_remap_option_add)
+	add_child(res_remap_option_file_open_dialog)
 
 func emit_localization_changed_signal() -> void:
-	emit_signal("localization_changed")
+	emit_signal(localization_changed)
