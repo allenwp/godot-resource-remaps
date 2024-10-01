041bf7c05b840622e2088c0c3be1d9a509dfe03c
Allen Pestaluky <allenpestaluky@gmail.com>
2024-10-01 09:15:40 -0400

    Combined scene and resource lists into one list because there's no reason for them to be separate

--- addons/resource_remaps/resource_remaps_export.gd
+++ addons/resource_remaps/resource_remaps_export.gd
@@ -4,7 +4,6 @@ class_name ResourceRemapPlugin extends EditorExportPlugin
 var _features: PackedStringArray
 
 var remap_resource: Dictionary
-var remap_scene: Dictionary
 var remap_file: Dictionary
 
 func _init() -> void:
@@ -19,13 +18,11 @@ func _init() -> void:
 		],
 		"res://audio-default.wav": [
 			["mobile", "res://audio-mobile.wav"],
-		]
-	}
-	remap_scene = {
+		],
 		"res://default.tscn": [
 			["ios", "res://ios.tscn"],
 			["mobile", "res://mobile.tscn"],
-		]
+		],
 	}
 	remap_file = {
 		"res://default.ogv": [
@@ -44,44 +41,7 @@ func _get_customization_configuration_hash() -> int:
 
 func _export_begin(features: PackedStringArray, _is_debug: bool, _path: String, _flags: int) -> void:
 	print("[Resource Remap] Remapping resources...")
-
 	_features = features
-
-func _begin_customize_resources (_platform: EditorExportPlatform, _f: PackedStringArray) -> bool:
-	return true
-
-func _customize_resource(_resource: Resource, path: String) -> Resource:
-	if remap_resource.has(path):
-		var feature_arrays: Array = remap_resource[path]
-		for feature_array: Array in feature_arrays:
-			var feature: String = feature_array[0]
-			if _features.has(feature):
-				var new_path: String = feature_array[1]
-				print("[Resource Remap] Resource: ", path, " to: ", new_path)
-				return load(new_path)
-	return null
-
-func _begin_customize_scenes(_platform: EditorExportPlatform, _f: PackedStringArray) -> bool:
-	return true
-
-func _customize_scene(scene: Node, path: String) -> Node:
-	if remap_scene.has(path):
-		var feature_arrays: Array = remap_scene[path]
-		for feature_array: Array in feature_arrays:
-			var feature: String = feature_array[0]
-			if _features.has(feature):
-				var new_path: String = feature_array[1]
-				var packed_scene: PackedScene = load(new_path as String) as PackedScene
-				if packed_scene != null && packed_scene.can_instantiate():
-					print("[Resource Remap] Scene: ", path, " to: ", new_path)
-					# Use GEN_EDIT_STATE_INSTANCE because that's what
-					# EditorExportPlatform::_export_customize uses to load a packed scene into a Node.
-					var result: Node = packed_scene.instantiate(PackedScene.GEN_EDIT_STATE_INSTANCE);
-					scene.free()
-					return result
-				else:
-					printerr("[Resource Remap] Failed Scene: ", path, " to: ", new_path)
-	return null
 
 func _export_file(path: String, _type: String, features: PackedStringArray) -> void:
 	if remap_file.has(path):
@@ -95,7 +55,6 @@ func _export_file(path: String, _type: String, features: PackedStringArray) -> v
 				break
 
 	if (remap_resource.has(path)
-		|| remap_scene.has(path)
 		|| remap_file.has(path)):
 		# Do not skip this path, even if it is listed as a new path to remap to.
 		# An example of this would be "vr" feature using the default resource as
@@ -132,14 +91,6 @@ func _export_file(path: String, _type: String, features: PackedStringArray) -> v
 				skip()
 				return
 
-	for feature_arrays: Array in remap_scene.values():
-		for feature_array: Array in feature_arrays:
-			var remapped_path: String = feature_array[1]
-			if remapped_path == path:
-				print("[Resource Remap] Skipping scene because it has been remapped: ", path)
-				skip()
-				return
-
 	for feature_arrays: Array in remap_file.values():
 		for feature_array: Array in feature_arrays:
 			var remapped_path: String = feature_array[1]
@@ -147,3 +98,39 @@ func _export_file(path: String, _type: String, features: PackedStringArray) -> v
 				print("[Resource Remap] Skipping file because it has been remapped: ", path)
 				skip()
 				return
+
+func _begin_customize_resources (_platform: EditorExportPlatform, _f: PackedStringArray) -> bool:
+	return true
+
+func _customize_resource(_resource: Resource, path: String) -> Resource:
+	if remap_resource.has(path):
+		var feature_arrays: Array = remap_resource[path]
+		for feature_array: Array in feature_arrays:
+			var feature: String = feature_array[0]
+			if _features.has(feature):
+				var new_path: String = feature_array[1]
+				print("[Resource Remap] Resource: ", path, " to: ", new_path)
+				return load(new_path)
+	return null
+
+func _begin_customize_scenes(_platform: EditorExportPlatform, _f: PackedStringArray) -> bool:
+	return true
+
+func _customize_scene(scene: Node, path: String) -> Node:
+	if remap_resource.has(path):
+		var feature_arrays: Array = remap_resource[path]
+		for feature_array: Array in feature_arrays:
+			var feature: String = feature_array[0]
+			if _features.has(feature):
+				var new_path: String = feature_array[1]
+				var packed_scene: PackedScene = load(new_path as String) as PackedScene
+				if packed_scene != null && packed_scene.can_instantiate():
+					print("[Resource Remap] Scene: ", path, " to: ", new_path)
+					# Use GEN_EDIT_STATE_INSTANCE because that's what
+					# EditorExportPlatform::_export_customize uses to load a packed scene into a Node.
+					var result: Node = packed_scene.instantiate(PackedScene.GEN_EDIT_STATE_INSTANCE);
+					scene.free()
+					return result
+				else:
+					printerr("[Resource Remap] Failed Scene: ", path, " to: ", new_path)
+	return null
