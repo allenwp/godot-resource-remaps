237531106e4ede44e62626bf9bfb0493b8d4a17d
Allen Pestaluky <allenpestaluky@gmail.com>
2024-10-22 13:05:54 -0400

    Updated license information

--- addons/resource_remaps/resource_remaps_control.gd
+++ addons/resource_remaps/resource_remaps_control.gd
@@ -1,6 +1,57 @@
-#TODO: add license and note about being copied from Godot source
+# MIT License
+#
+# Copyright (c) 2024 Allen Pestaluky
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
 # This class was originally copied from the LocalizationEditor C++ class of Godot.
-# Much of the structure and naming follows the names of that class.
+# Much of the structure and naming follows the names of that class. Original license:
+#/**************************************************************************/
+#/*  localization_editor.cpp                                               */
+#/**************************************************************************/
+#/*                         This file is part of:                          */
+#/*                             GODOT ENGINE                               */
+#/*                        https://godotengine.org                         */
+#/**************************************************************************/
+#/* Copyright (c) 2014-present Godot Engine contributors (see AUTHORS.md). */
+#/* Copyright (c) 2007-2014 Juan Linietsky, Ariel Manzur.                  */
+#/*                                                                        */
+#/* Permission is hereby granted, free of charge, to any person obtaining  */
+#/* a copy of this software and associated documentation files (the        */
+#/* "Software"), to deal in the Software without restriction, including    */
+#/* without limitation the rights to use, copy, modify, merge, publish,    */
+#/* distribute, sublicense, and/or sell copies of the Software, and to     */
+#/* permit persons to whom the Software is furnished to do so, subject to  */
+#/* the following conditions:                                              */
+#/*                                                                        */
+#/* The above copyright notice and this permission notice shall be         */
+#/* included in all copies or substantial portions of the Software.        */
+#/*                                                                        */
+#/* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,        */
+#/* EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF     */
+#/* MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. */
+#/* IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY   */
+#/* CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,   */
+#/* TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE      */
+#/* SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.                 */
+#/**************************************************************************/
+
 class_name ResourceRemapControl extends VBoxContainer
 
 var res_remap_option_add_button: Button = null
